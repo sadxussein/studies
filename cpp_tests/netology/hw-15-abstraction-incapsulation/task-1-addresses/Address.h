1014a47a241e6dfc848fbbbbd70601529e24cb14
sadxussein <kvdworking@gmail.com>
2024-03-07 17:00:19 +0300

    need to finish work on hw-15

---
+++ cpp_tests/netology/hw-15-abstraction-incapsulation/task-1-addresses/Address.h
@@ -0,0 +1,15 @@
+#ifndef TASK_1_ADDRESSES_ADDRESS_H
+#define TASK_1_ADDRESSES_ADDRESS_H
+
+
+class Address
+{
+private:
+    /* data */
+public:
+    Address(/* args */);
+    ~Address();
+};
+
+
+#endif //TASK_1_ADDRESSES_ADDRESS_H
