cfddd3a00ca5ceca3d07af2ccf2f7e1c64353932
xussein <kvdworking@gmail.com>
2024-03-26 22:40:21 +0300

    hw 20 completed

---
+++ cpp_tests/netology/hw-20-operator-overloading/task-1-fraction-comparison/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+class Fraction
+{
+private:
+	int numerator_;
+	int denominator_;
+
+public:
+	Fraction(int numerator, int denominator)
+	{
+		numerator_ = numerator;
+		denominator_ = denominator;
+	}
+    bool operator<(Fraction & other) const { return numerator_ * other.denominator_ < other.numerator_ * denominator_; }
+    bool operator>(Fraction & other) const { return numerator_ * other.denominator_ > other.numerator_ * denominator_; }
+    bool operator<=(Fraction & other) const { return numerator_ * other.denominator_ <= other.numerator_ * denominator_; }
+    bool operator>=(Fraction & other) const { return numerator_ * other.denominator_ >= other.numerator_ * denominator_; }
+    bool operator==(Fraction & other) const { return numerator_ * other.denominator_ == other.numerator_ * denominator_; }
+    bool operator!=(Fraction & other) const { return numerator_ * other.denominator_ != other.numerator_ * denominator_; }
+    
+};
+
+int main()
+{
+	Fraction f1(4, 3);
+	Fraction f2(6, 11);
+
+	std::cout << "f1" << ((f1 == f2) ? " == " : " not == ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 != f2) ? " != " : " not != ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 < f2) ? " < " : " not < ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 > f2) ? " > " : " not > ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 <= f2) ? " <= " : " not <= ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 >= f2) ? " >= " : " not >= ") << "f2" << '\n';
+	return 0;
+}
