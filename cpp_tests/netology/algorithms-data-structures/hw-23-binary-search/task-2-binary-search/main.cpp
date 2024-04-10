c57169e063366f2352740d49c7cd73755ec1e51f
xussein <kvdworking@gmail.com>
2024-04-10 22:20:06 +0300

    hw 23 completed

--- cpp_tests/netology/algorithms-data-structures/hw-23-binary-search/task-2-binary-search/main.cpp
+++ cpp_tests/netology/algorithms-data-structures/hw-23-binary-search/task-2-binary-search/main.cpp
@@ -1,10 +1,49 @@
 #include <iostream>
-#include <random>
+#include <vector>
+#include <algorithm>
+
+size_t sort(int separator, std::vector<int> & values) {
+    size_t left = 0;
+    size_t right = values.size() - 1;
+    size_t result = -1;
+    while (left <= right && result == -1) {
+        size_t middle = left + (right - left) / 2;
+        if (separator > values[middle]) {
+            left = middle + 1;
+        } else if (separator < values[middle]) {
+            right = middle - 1;
+        } else {
+            result = middle;
+        }
+    }
+
+    if (result != -1) {
+        while (result < values.size() - 1 && values[result] == values[result + 1]) {
+            result ++;
+        }
+    }
+
+    if (result != -1) {
+        return values.size() - result - 1;
+    } else {
+        return values.size() - right - 1;
+    }
+}
 
 int main() {
-    std::cout << "Input separation point: " << std::endl;
-    int separation;
-    std::cin >> separation;
-    std::cout << std::rand() << ' ' << RAND_MAX << std::endl;
+    std::cout << "Enter values separated by spaces: ";
+    std::vector<int> values;
+    int value;
+    while (std::cin >> value) {
+        values.push_back(value);
+        if (std::cin.peek() == '\n') {
+            break;
+        }
+    }
+    std::sort(values.begin(), values.end());
+    std::cout << "Enter separation point: ";
+    int separator;
+    std::cin >> separator;
+    std::cout << "Number of elements, greater than " << separator << " is " << sort(separator, values) << '.' << std::endl;
     return 0;
 }
